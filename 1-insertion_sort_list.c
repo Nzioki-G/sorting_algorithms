d7becddfa13d70a2da75d85f554f72bccd383372
Grace <nzioki.grace@gmail.com>
2023-09-21 13:25:48 +0300

    2 sides: sorted, unsorted. Expand sorted side until the end

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,29 +5,28 @@
  * element, take it to its correct position then continue where you left off
  * @list: a doubly linked list of integer
 */
-
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *key;
 	listint_t *current;
 
 	current = *list;
-	/* current = current->next; */
 	while (current)
 	{
 		key = current;
+
+		/* take a mis-positioned element back node by node */
 		while (current->prev && (current->prev->n > current->n))
 		{
 			/* swap */
 			swap_nodes(current->prev, current);
+
 			/* reseting node at head */
 			if (current->prev == NULL)
 				*list = current;
 			print_list(*list);
 		}
-		
+		/* here's where we were */
 		current = key->next;
-	
-		/* current = key->next */;
 	}
 }
